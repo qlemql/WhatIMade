d37eda31d48265776bcef815b57b250e7dc37df4
qlemql <busgamer@naver.com>
2020-06-11 06:14:41 +0900

    set up js
    grid와 score start-btn const 생성

---
+++ app.js
@@ -0,0 +1,5 @@
+const grid = document.querySelector(".grid");
+let squares = Array.from(document.querySelectorAll(".grid div"));
+const ScoreDisplay = document.querySelector("#score");
+const StartBtn = document.querySelector("start-btn");
+const width = 10;
