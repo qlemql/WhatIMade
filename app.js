dca5236703e0087c700e27f4dd7f134e963bf1f7
qlemql <busgamer@naver.com>
2020-06-12 15:13:10 +0900

    Drawing Tetrominoes
    Array로 테트리스 도형 만들기
    Math랜덤으로 만든 모양이 랜덤하게 나오기
    forEach로 Array안에 있는 모든 도형들에게 적용

--- app.js
+++ app.js
@@ -3,3 +3,65 @@ let squares = Array.from(document.querySelectorAll(".grid div"));
 const ScoreDisplay = document.querySelector("#score");
 const StartBtn = document.querySelector("start-btn");
 const width = 10;
+
+const lTetromino = [
+  [1, width + 1, width * 2 + 1, 2],
+  [width, width + 1, width + 2, width * 2 + 2],
+  [1, width + 1, width * 2 + 1, width * 2],
+  [width, width * 2, width * 2 + 1, width * 2 + 2],
+];
+
+const zTetrominoe = [
+  [0, width, width + 1, width * 2 + 1],
+  [width + 1, width + 2, width * 2, width * 2 + 1],
+  [0, width, width + 1, width * 2 + 1],
+  [width + 1, width + 2, width * 2, width * 2 + 1],
+];
+
+const tTetrominoe = [
+  [1, width, width + 1, width + 2],
+  [1, width + 1, width + 2, width * 2 + 1],
+  [width, width + 1, width + 2, width * 2 + 1],
+  [1, width, width + 1, width * 2 + 1],
+];
+
+const oTetrominoe = [
+  [0, 1, width, width + 1],
+  [0, 1, width, width + 1],
+  [0, 1, width, width + 1],
+  [0, 1, width, width + 1],
+];
+
+const iTetrominoe = [
+  [1, width + 1, width * 2 + 1, width * 3 + 1],
+  [width, width + 1, width + 2, width + 3],
+  [1, width + 1, width * 2 + 1, width * 3 + 1],
+  [width, width + 1, width + 2, width + 3],
+];
+
+const theTetrominoes = [
+  lTetromino,
+  zTetrominoe,
+  tTetrominoe,
+  oTetrominoe,
+  iTetrominoe,
+];
+
+let currentPosition = 4;
+let currentRotation = 0;
+let random = Math.floor(Math.random() * theTetrominoes.length);
+let current = theTetrominoes[random][0];
+
+function draw() {
+  current.forEach((index) => {
+    squares[currentPosition + index].classList.add("tetromino");
+  });
+}
+
+function undraw() {
+  current.forEach((index) => {
+    squares[currentPosition + index].classList.remove("tetromino");
+  });
+}
+
+draw();
