8b21514d52abf4cfec15a0984e1b3dd6116c24c1
qlemql <busgamer@naver.com>
2020-08-08 22:25:11 +0900

    feat&docs
    함수 add에 주석 추가
    
    함수 calculate 생성
    eval함수는 js에 내장된 함수로 식을 입력 받으면 그 식을 계산해서 결과를 알려줌.
    
    함수 calculate와 onclick을 연결

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 function add(char) {
   var display = document.getElementById("display");
+  //display.value는 input 태그 안에 들어 있는 값을 의미.
   display.value = display.value + char;
+  //input 태그의 값 뒤에 char 문자를 추가해 주는 역할.
+}
+
+function calculate() {
+  var display = document.getElementById("display");
+  var result = eval(display.value);
+  document.getElementById("result").value = result;
 }
