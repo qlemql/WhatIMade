8d3b795e0eb7f9d75391cd2eddafbe061afbb292
qlemql <busgamer@naver.com>
2021-03-24 23:17:19 +0900

    Create React App
    stlyed-components 사용
    [따로 css파일 만들지않고 해당 js파일에서 스타일 처리]
    
    @material-ui/core, icon 사용
    [font awsome 대신 사용]

---
+++ src/components/Header.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import styled from "styled-components";
+
+function Header() {
+    return (
+        <Container>
+            <Main>
+
+            </Main>
+            {/* <UserContainer>
+
+            </UserContainer> */}
+        </Container>
+    )
+}
+
+export default Header
+
+
+const Container = styled.div`
+
+`
+
+const Main = styled.div`
+
+`
